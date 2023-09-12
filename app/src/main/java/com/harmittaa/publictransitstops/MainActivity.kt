c1320851211997d608bb1b97341fdeb1d6cc4ebe
harmittaa <matti.makikihnia@gmail.com>
2023-09-12 15:43:14 +0300

    Initial commit

---
+++ app/src/main/java/com/harmittaa/publictransitstops/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.harmittaa.publictransitstops
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.harmittaa.publictransitstops.ui.theme.PublicTransitStopsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PublicTransitStopsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PublicTransitStopsTheme {
+        Greeting("Android")
+    }
+}
